00b37361a8961e24f2f2f42d4f583e8b53a6bda1
Elian Mariano <elianmarianogabriel@gmail.com>
2022-06-03 16:28:55 -0300

    Project Structure

---
+++ Models/Author.cs
@@ -0,0 +1,7 @@
+namespace LibraryAPI.Models;
+
+public class Author : Client {
+    public String profission { get; set; }
+
+    public String authorDescription { get; set; }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+namespace LibraryAPI.Models;
+
+public class Category{
+    public int id { get; set; }
+
+    public String name { get; set; }
+
+    public String description { get; set; }
+
+    public List<Book>? books { get; set; }
+}
